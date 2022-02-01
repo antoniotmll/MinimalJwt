73625b63801206aeec13fe8edcc73f3c23ee96c6
Antonio <antoniotmll@gmail.com>
2022-02-01 21:46:54 +0100

    Added user repository

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using MinimalJwt.Models;
+
+namespace MinimalJwt.Repositories
+{
+    public class UserRepository
+    {
+        public static List<User> Users = new()
+        {
+            new()
+            {
+                UserName = "Antonio",
+                EmailAdress = "antonio@mail.com",
+                Password = "1234",
+                GivenName = "Antuan",
+                Surename = "Tomas",
+                Role = "Admin"
+            },
+
+            new()
+            {
+                UserName = "Araceli",
+                EmailAdress = "araceli@mail.com",
+                Password = "Hola",
+                GivenName = "Ara",
+                Surename = "Adsuar",
+                Role = "Standard"
+            }
+        };
+    }
+}
