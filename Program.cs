858398ab48152741f91501d4f0cd3209c340b61a
Antonio <antoniotmll@gmail.com>
2022-02-02 22:51:03 +0100

    API endpoint configuration

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using MinimalJwt.Models;
+using MinimalJwt.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<IMovieService, MovieService>();
+builder.Services.AddSingleton<IUserService, UserService>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+//app.MapPost("/create",
+    //(Movie movie, IMovieService service) => Create(movie, service));
+
 app.Run();
+
