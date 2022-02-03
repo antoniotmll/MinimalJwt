205265ac7415edc32d03699b6d3d6b80197c9af0
Antonio <antoniotmll@gmail.com>
2022-02-03 22:03:17 +0100

    Added all API endpoints

--- Program.cs
+++ Program.cs
@@ -14,11 +14,55 @@ app.MapGet("/", () => "Hello World!");
 app.MapPost("/create",
     (Movie movie, IMovieService service) => Create(movie, service));
 
+app.MapGet("/get",
+    (int id, IMovieService service) => Get(id, service));
+
+app.MapGet("/list",
+    (IMovieService service) => List(service));
+
+app.MapPut("/update",
+    (Movie newMovie, IMovieService service) => Update(newMovie, service));
+
 IResult Create(Movie movie, IMovieService service)
 {
     var result = service.Create(movie);
     return Results.Ok(result);
 }
+
+IResult Get(int id, IMovieService service)
+{
+    var movie = service.Get(id);
+
+    if (movie is null) return Results.NotFound("Movie not found");
+    
+    return Results.Ok(movie);
+}
+
+IResult List(IMovieService service)
+{
+    var movies = service.List();
+
+    return Results.Ok(movies);
+}
+
+IResult Update(Movie newMovie, IMovieService service)
+{
+    var updateMovie = service.Update(newMovie);
+
+    if (updateMovie is null) return Results.NotFound("Movie not found");
+
+    return Results.Ok(updateMovie);
+}
+
+IResult Delete(int id, IMovieService service)
+{
+    var result = service.Delete(id);
+
+    if (!result) Results.BadRequest("Something wrong!");
+
+    return Results.Ok(result); 
+}
+
 
 app.Run();
 
